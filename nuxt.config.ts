6666529fd6a2511832213846c4306e2aa9fa9531
urvish mandaliya <115796297+aUrvish@users.noreply.github.com>
2025-04-19 15:35:31 +0530

    Add Geist Mono font and layout file (#2)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&display=swap'
+        }
+      ]
+    }
+  },
   modules: ['@nuxtjs/tailwindcss'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Geist Mono"', 'monospace'],
+      }
+    },
   },
   plugins: [],
 }
