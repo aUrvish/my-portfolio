c3cca3543c7910c5c98d80d50912eb79856b637e
urvish mandaliya <115796297+aUrvish@users.noreply.github.com>
2025-06-07 23:36:34 +0530

    Add my studio section (#9)
    * Add my studio section
    
    * Update hero and studio section
    
    ---------
    
    Co-authored-by: Ashish <ajmandaliya290@gmail.com>

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -1,71 +1,73 @@
 <template>
-    <div class="md:py-28 py-8 flex md:flex-row flex-col justify-between items-center md:gap-12 gap-6">
-        <div class="md:max-w-[280px] max-w-[180px] relative">
-            <img src="../../assets/images/me.jpg"
-                class="w-full aspect-square md:rounded-3xl rounded-2xl border-2 p-1 dark:border-neutral-800"
-                alt="Urvish Mandaliya" />
-            <p
-                class="absolute right-0 bottom-0 md:text-2xl text-xl p-2 dark:bg-black bg-white rounded-full translate-x-1/4 translate-y-1/4 dark:border-neutral-800 border-2">👋</p>
-        </div>
-        <div class="max-w-2xl">
-            <!-- <h1>Hero component</h1> -->
-            <p class="capitalize dark:text-neutral-400 text-neutral-700 py-1.5 rounded-md md:px-4 px-2.5 border w-fit font-semibold dark:border-neutral-800 md:text-base text-sm"
-                ><span ref="typedText"></span></p>
-            <p class="dark:text-white mt-2 font-black md:text-[34px] text-[22px]">Hey, I'm Urvish Mandaliya</p>
-            <div class="w-24 bg-blue-200 dark:bg-blue-200/10 h-1 rounded-md overflow-hidden" >
-                <div class="h-full w-1/3 bg-blue-500 rounded-md loading-bar" ></div>
-            </div>
-            <p class="dark:text-neutral-400 md:text-base text-sm mt-4">
-                I'm a passionate Full Stack Web Developer with 2 years of experience in building dynamic and scalable web apps using Vue.js and Laravel. I craft clean, responsive UIs and develop robust backends. I love solving real-world problems with code and constantly strive to improve and grow my skills 🚀.
-            </p>
+  <div
+    class="md:py-28 sm:py-20 py-12 flex md:flex-row flex-col justify-between items-center md:gap-12 gap-6"
+  >
+    <div>
+      <div
+        class="flex sm:flex-row flex-col-reverse md:items-end md:gap-4 gap-3"
+      >
+        <p
+          class="dark:text-white mt-2 font-black md:text-5xl sm:text-4xl text-3xl leading-none"
+        >
+          Hey👋, I'm <span class="text-blue-500">Urvish</span>
+        </p>
+        <img
+          src="../../assets/images/me.jpg"
+          class="w-full max-w-12 aspect-square rounded-full border border-neutral-400"
+          alt="Urvish Mandaliya"
+        />
+      </div>
+      <p
+        class="dark:text-white mt-2 font-black lg:text-5xl sm:text-4xl text-xl"
+      >
+        a Full-Stack Web Developer
+      </p>
+      <p
+        class="dark:text-neutral-400 md:text-lg md:leading-relaxed text-base md:mt-6 mt-4"
+      >
+        I'm a passionate
+        <span
+          class="dark:bg-white/10 bg-neutral-200 dark:text-neutral-300 text-neutral-700 leading-tight font-medium px-1.5 rounded-md"
+        >
+          Full Stack Web Developer</span
+        >
+        with
+        <span
+          class="dark:bg-white/10 bg-neutral-200 dark:text-neutral-300 text-neutral-700 leading-tight font-medium px-1.5 rounded-md"
+          >2+ years</span
+        >
+        of experience in building dynamic and scalable web applications using
+        <span
+          class="dark:bg-white/10 bg-neutral-200 leading-tight font-medium px-1.5 rounded-md dark:text-neutral-300 text-neutral-700"
+          >Vue.js</span
+        >
+        and
+        <span
+          class="dark:bg-white/10 bg-neutral-200 leading-tight font-medium px-1.5 rounded-md dark:text-neutral-300 text-neutral-700"
+        >
+          Laravel</span
+        >. I specialize in crafting clean, responsive user interfaces and
+        developing robust, secure backends. I love solving real-world problems
+        with code, exploring new technologies, building innovative solutions,
+        and constantly pushing myself to grow and improve every day.
+      </p>
 
-            <div class="mt-6 flex items-center md:gap-4 gap-2">
-                <button
-                class="md:py-[5px] py-1 flex items-center gap-2 px-3 cursor-pointer rounded-md bg-blue-500 hover:bg-blue-600 text-neutral-100 border-2 border-blue-500 hover:border-blue-600">
-                    <Users class="md:w-[18px] w-4" />
-                    <p class="capitalize md:text-base text-[12px]">Contact me</p>
-                </button>
-                <button
-                class="md:py-[5px] py-1 flex items-center gap-2 px-3 cursor-pointer rounded-md border-2 border-blue-500 text-blue-500">
-                    <ArrowDownToLine class="md:w-[18px] w-4" />
-                    <p class="capitalize md:text-base text-[12px]">Download Resume</p>
-                </button>
-            </div>
-        </div>
+      <div class="mt-6 flex items-center md:gap-4 gap-2">
+        <button
+          class="md:py-2 py-1.5 flex items-center gap-3 px-3.5 cursor-pointer rounded-md bg-blue-500 hover:bg-blue-600 text-neutral-100"
+        >
+          <Users class="md:w-5 w-4" />
+          <p class="uppercase font-medium md:text-base text-sm">Contact me</p>
+        </button>
+        <button
+          class="md:py-2 py-1.5 flex items-center gap-2 px-3 cursor-pointer rounded-md bg-neutral-200 dark:bg-white/10 dark:hover:bg-white/15 text-neutral-700 dark:text-neutral-100"
+        >
+          <ArrowDownToLine class="md:w-5 w-4" />
+          <p class="uppercase font-medium md:text-base text-sm">
+            Download Resume
+          </p>
+        </button>
+      </div>
     </div>
+  </div>
 </template>
-
-<script setup>
-import Typed from 'typed.js';
-
-const typedText = ref(null)
-
-const handleTypeAnimation = () => {
-    const options = {
-        strings: ["", "Full stack web developer", "Laravel Developer", "Web Designer", "Vue.js Developer"],
-        typeSpeed: 100,
-        backSpeed: 40,
-        loop: true,
-    }
-
-    new Typed(typedText.value, options)
-}
-onMounted(() => {
-    handleTypeAnimation()
-})
-</script>
-
-<style>
-  @keyframes loadingSlide {
-    0% {
-      transform: translateX(-100%);
-    }
-    100% {
-      transform: translateX(300%);
-    }
-  }
-
-  .loading-bar {
-    animation: loadingSlide 3s linear infinite;
-  }
-</style>
---
+++ components/Home/MyStudio.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="grid md:grid-cols-2 sm:gap-6 gap-4 py-8">
+    <div
+      class="dark:bg-white/5 bg-black/5 md:p-6 p-4 rounded-lg md:border-2 border border-neutral-200 dark:border-neutral-700"
+    >
+      <Laptop
+        class="md:w-12 md:h-12 w-10 h-10 dark:text-neutral-100 text-neutral-700"
+      />
+      <h6
+        class="md:text-xl text-lg md:mt-4 mt-3 dark:text-neutral-100 font-semibold"
+      >
+        Full Stack web development
+      </h6>
+      <p
+        class="md:text-base text-sm md:mt-2 mt-1 dark:text-neutral-300 text-neutral-600 font-normal"
+      >
+        Building high-performance, scalable websites with clean, maintainable
+        code — leveraging Vue.js for dynamic front-ends and Laravel for secure,
+        efficient back-end development.
+      </p>
+    </div>
+    <div
+      class="dark:bg-white/5 bg-black/5 md:p-6 p-4 rounded-lg md:border-2 border border-neutral-200 dark:border-neutral-700"
+    >
+      <Palette
+        class="md:w-12 md:h-12 w-10 h-10 dark:text-neutral-100 text-neutral-700"
+      />
+      <h6
+        class="md:text-xl text-lg md:mt-4 mt-3 dark:text-neutral-100 font-semibold"
+      >
+        Web design
+      </h6>
+      <p
+        class="md:text-base text-sm md:mt-2 mt-1 dark:text-neutral-300 text-neutral-600 font-normal"
+      >
+        Crafting modern, intuitive user interfaces with Tailwind CSS that are
+        highly responsive, visually appealing, and performance-optimized to
+        engage and convert users
+      </p>
+    </div>
+  </div>
+</template>
