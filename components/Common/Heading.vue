91e61b8a5098eb45d4461634bc864ba1cbb00e95
amandaliya29 <168665452+amandaliya29@users.noreply.github.com>
2025-07-16 23:11:01 +0530

    Add heading component (#16)

---
+++ components/Common/Heading.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="border-l-[3px] border-blue-500 md:pl-7 pl-4 flex md:gap-4 gap-3 items-center md:py-1.5 py-1"
+  >
+    <p class="md:text-2xl text-xl font-semibold text-blue-500 capitalize">
+      {{ title }}<span class="dark:text-white text-black">.</span>
+    </p>
+  </div>
+  <p
+    class="md:pl-8 md:mt-0 mt-2 dark:text-neutral-400 text-neutral-500 leading-tight font-instrument font-extralight md:tracking-wider"
+    v-if="caption"
+  >
+    {{ caption }}
+  </p>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    require: true,
+  },
+  caption: {
+    default: null,
+  },
+});
+</script>
