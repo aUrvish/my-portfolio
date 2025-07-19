d10d1cda9e8f0680a1dc36a14819faa418707141
Ashish <ajmandaliya290@gmail.com>
2025-07-19 21:43:16 +0530

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,21 @@
 <template>
-    <Navbar />
-    <section>
-        <slot />
-    </section>
+  <Navbar />
+  <section class="px-4">
+    <div
+      class="max-w-screen-xl mx-auto md:border-l dark:border-neutral-700 border-neutral-300"
+    >
+      <slot />
+      <Bottom />
+    </div>
+  </section>
 </template>
 
-<script setup >
-const themeStore = useThemeStore()
+<script setup>
+const themeStore = useThemeStore();
 
 useHead(() => ({
   htmlAttrs: {
-    class: themeStore.getTheme().is_dark ? 'dark' : '',
+    class: themeStore.getTheme().is_dark ? "dark" : "",
   },
-}))
+}));
 </script>
