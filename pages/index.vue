9e7db25dac8aa94abe560c9dc1270510e2ec1116
amandaliya29 <168665452+amandaliya29@users.noreply.github.com>
2025-07-19 21:44:07 +0530

    Add footer (#19)

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,9 @@
 <template>
-  <section class="px-4">
-    <div
-      class="max-w-screen-xl mx-auto md:border-l dark:border-neutral-700 border-neutral-300"
-    >
-      <HomeHero />
-      <HomeAbout />
-      <CommonProjects class="md:py-32 py-24" />
-      <CommonExperience class="py-4" />
-      <CommonSkills class="md:py-32 py-24" />
-      <CommonCertificate class="py-4" />
-      <CommonContact class="md:py-32 py-24" />
-    </div>
-  </section>
+  <HomeHero />
+  <HomeAbout />
+  <CommonProjects class="md:py-32 py-24" />
+  <CommonExperience class="py-4" />
+  <CommonSkills class="md:py-32 py-24" />
+  <CommonCertificate class="py-4" />
+  <CommonContact class="md:py-32 py-24" />
 </template>
