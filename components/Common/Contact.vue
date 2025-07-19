83fa1ef6cc89348d716639ff41c90dcc3ed892fb
amandaliya29 <168665452+amandaliya29@users.noreply.github.com>
2025-07-19 23:07:56 +0530

    Integrated fromspress (#20)

--- components/Common/Contact.vue
+++ components/Common/Contact.vue
@@ -6,10 +6,15 @@
     />
     <div class="md:pl-8 mt-8">
       <form @submit="onSubmit">
-        <div class="md:p-6 p-4 border border-neutral-200 dark:border-neutral-800 rounded-sm md:shadow-md shadow bg-neutral-100 dark:bg-[#191919]">
+        <div
+          class="md:p-6 p-4 border border-neutral-200 dark:border-neutral-800 rounded-sm md:shadow-md shadow bg-neutral-100 dark:bg-[#191919]"
+        >
           <div class="grid md:grid-cols-2 md:gap-6 gap-4">
             <div class="flex flex-col gap-2">
-              <label for="name" class="md:text-lg text-base font-normal text-neutral-500 dark:text-neutral-400">
+              <label
+                for="name"
+                class="md:text-lg text-base font-normal text-neutral-500 dark:text-neutral-400"
+              >
                 Name<span class="text-red-500">*</span>
               </label>
               <input
@@ -23,7 +28,10 @@
             </div>
 
             <div class="flex flex-col gap-2">
-              <label for="email" class="md:text-lg text-base font-normal text-neutral-500 dark:text-neutral-400">
+              <label
+                for="email"
+                class="md:text-lg text-base font-normal text-neutral-500 dark:text-neutral-400"
+              >
                 Email<span class="text-red-500">*</span>
               </label>
               <input
@@ -38,7 +46,10 @@
           </div>
 
           <div class="flex flex-col gap-2 mt-4">
-            <label for="message" class="md:text-lg text-base font-normal text-neutral-500 dark:text-neutral-400">
+            <label
+              for="message"
+              class="md:text-lg text-base font-normal text-neutral-500 dark:text-neutral-400"
+            >
               Message<span class="text-red-500">*</span>
             </label>
             <textarea
@@ -53,20 +64,26 @@
 
           <div class="flex lg:flex-row flex-col lg:items-center gap-6 mt-6">
             <div>
-                <button
+              <button
                 type="submit"
                 class="flex gap-2 items-center md:text-lg text-base text-neutral-500 rounded-sm border hover:bg-neutral-100 bg-neutral-200 dark:bg-neutral-800 dark:border-neutral-800 transition-all md:py-1 py-1.5 px-3"
-                >
+              >
                 <Send class="md:w-5 w-4" />
                 Send
-                </button>
+              </button>
             </div>
             <transition name="fade">
-                <p class="md:text-base dark:text-neutral-200 text-neutral-800 text-sm tracking-tight flex items-center gap-3 " v-show="responseBadge.show">
-                    <CircleX v-if="responseBadge.type == 'error'" class="sm:w-5 w-10 text-red-500" />
-                    <CircleCheckBig v-else class="sm:w-5 w-10 text-green-600" />
-                    <span>{{ responseBadge.message }}</span>
-                </p>
+              <p
+                class="md:text-base dark:text-neutral-200 text-neutral-800 text-sm tracking-tight flex items-center gap-3"
+                v-show="responseBadge.show"
+              >
+                <CircleX
+                  v-if="responseBadge.type == 'error'"
+                  class="sm:w-5 w-10 text-red-500"
+                />
+                <CircleCheckBig v-else class="sm:w-5 w-10 text-green-600" />
+                <span>{{ responseBadge.message }}</span>
+              </p>
             </transition>
           </div>
         </div>
@@ -77,45 +94,50 @@
 
 <script setup>
 const schema = object({
-  name: string().required('Name is required'),
-  email: string().required('Email is required').email('Invalid email'),
-  message: string().required('Message is required'),
-})
+  name: string().required("Name is required"),
+  email: string().required("Email is required").email("Invalid email"),
+  message: string().required("Message is required"),
+});
 
 const responseBadge = ref({
-    type : "error",
-    message : "",
-    show : false
-})
+  type: "error",
+  message: "",
+  show: false,
+});
 
-const { handleSubmit, resetForm } = useForm({ validationSchema: schema })
+const { handleSubmit, resetForm } = useForm({ validationSchema: schema });
 
-const { value: name, errorMessage: nameError } = useField('name')
-const { value: email, errorMessage: emailError } = useField('email')
-const { value: message, errorMessage: messageError } = useField('message')
+const { value: name, errorMessage: nameError } = useField("name");
+const { value: email, errorMessage: emailError } = useField("email");
+const { value: message, errorMessage: messageError } = useField("message");
 
-const handleResponseBadge = (message, type = 'error') => {
-    responseBadge.value.type = type
-    responseBadge.value.message = message
-    responseBadge.value.show = true
+const handleResponseBadge = (message, type = "error") => {
+  responseBadge.value.type = type;
+  responseBadge.value.message = message;
+  responseBadge.value.show = true;
 
-    setTimeout(() => {
-       responseBadge.value.show = false
-    }, 5000)
-}
+  setTimeout(() => {
+    responseBadge.value.show = false;
+  }, 5000);
+};
 
 const onSubmit = handleSubmit(async (values) => {
   try {
-    const url = `https://formsubmit.co/${useRuntimeConfig().public.formSubmitKey}`
+    const url = `https://formspree.io/f/${
+      useRuntimeConfig().public.formSubmitKey
+    }`;
     await $fetch(url, {
-      method: 'POST',
+      method: "POST",
       body: values,
-    })
+    });
 
-    handleResponseBadge("Got it! I’ll reach out to you as soon as possible.", "success");
-    resetForm()
+    handleResponseBadge(
+      "Got it! I’ll reach out to you as soon as possible.",
+      "success"
+    );
+    resetForm();
   } catch (error) {
     handleResponseBadge("Something went wrong. Please try again later.");
   }
-})
+});
 </script>
