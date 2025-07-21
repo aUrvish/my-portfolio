bfcc83335fb95ee612ea873b8c388b9a410197ee
amandaliya29 <168665452+amandaliya29@users.noreply.github.com>
2025-07-21 23:10:23 +0530

    hero section changes (#22)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <Navbar />
-  <section class="px-4">
+  <section class="md:px-8 px-4">
     <div
       class="max-w-screen-xl mx-auto md:border-l dark:border-neutral-700 border-neutral-300"
     >
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,12 @@ export default defineNuxtConfig({
           href: "/favicon.png",
         },
       ],
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+      ],
     },
   },
   css: ["@/assets/css/main.css"],
@@ -38,58 +44,58 @@ export default defineNuxtConfig({
   imports: {
     imports: [
       {
-        from: 'vee-validate',
-        name: 'useForm',
-        as: 'useForm',
+        from: "vee-validate",
+        name: "useForm",
+        as: "useForm",
       },
       {
-        from: 'vee-validate',
-        name: 'useField',
-        as: 'useField',
+        from: "vee-validate",
+        name: "useField",
+        as: "useField",
       },
       {
-        from: 'vee-validate',
-        name: 'useFieldArray',
-        as: 'useFieldArray',
+        from: "vee-validate",
+        name: "useFieldArray",
+        as: "useFieldArray",
       },
       {
-        from: 'vee-validate',
-        name: 'defineRule',
-        as: 'defineRule',
+        from: "vee-validate",
+        name: "defineRule",
+        as: "defineRule",
       },
       {
-        from: 'yup',
-        name: 'object',
-        as: 'object',
+        from: "yup",
+        name: "object",
+        as: "object",
       },
       {
-        from: 'yup',
-        name: 'string',
-        as: 'string',
+        from: "yup",
+        name: "string",
+        as: "string",
       },
       {
-        from: 'yup',
-        name: 'number',
-        as: 'number',
+        from: "yup",
+        name: "number",
+        as: "number",
       },
       {
-        from: 'yup',
-        name: 'boolean',
-        as: 'boolean',
+        from: "yup",
+        name: "boolean",
+        as: "boolean",
       },
       {
-        from: 'yup',
-        name: 'array',
-        as: 'array',
+        from: "yup",
+        name: "array",
+        as: "array",
       },
     ],
   },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   runtimeConfig: {
-    public : {
-      crispWebsiteId : process.env.CRISP_WEBSITE_ID,
-      formSubmitKey : process.env.FORM_SUBMIT_KEY
-    }
-  }
+    public: {
+      crispWebsiteId: process.env.CRISP_WEBSITE_ID,
+      formSubmitKey: process.env.FORM_SUBMIT_KEY,
+    },
+  },
 });
