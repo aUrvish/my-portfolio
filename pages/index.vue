7b6c2f8538cdef171672ac4a8c6b68ec25b1c118
Ashish <ajmandaliya290@gmail.com>
2025-07-22 23:19:57 +0530

    Add contact page

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,5 @@
   <CommonExperience class="py-4" />
   <CommonSkills class="md:py-32 py-24" />
   <CommonCertificate class="py-4" />
-  <CommonContact class="md:py-32 py-24" />
+  <HomeContact class="md:py-32 py-24" />
 </template>
