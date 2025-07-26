79b7759b298530579bbecf04cd54ad6793faf223
Mandaliya Urvish <mandaliyaurvishj7@gmail.com>
2025-07-26 23:27:30 +0530

    Add project page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <HomeHero />
   <HomeAbout />
-  <CommonProjects class="md:py-32 py-24" />
+  <HomeProjects class="md:py-32 py-24" />
   <CommonExperience class="py-4" />
   <CommonSkills class="md:py-32 py-24" />
   <CommonCertificate class="py-4" />
